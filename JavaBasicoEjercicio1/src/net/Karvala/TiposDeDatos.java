53dd4e9bb0474a18485c6f59e42321fd99bf6e04
Sara <77838356+Karvala3@users.noreply.github.com>
2021-12-29 18:20:45 +0100

    Add files via upload

---
+++ JavaBasicoEjercicio1/src/net/Karvala/TiposDeDatos.java
@@ -0,0 +1,44 @@
+package net.Karvala;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+
+        /* Numéricos
+           - Enteros   */
+        byte variable1 = 1;
+        short variable2 = 2;
+        int variable3 = 4;
+        long variable4 = 8;
+
+        // - Decimales
+        float variable5 = 4.9f;
+        double variable6 = 4.99d;
+
+        // Boleanos
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        /* Texto
+           - Carácter  */
+        char variable9 = 'a';
+
+        // - Cadenas de texto
+        String variable10 = "cadena de texto";
+
+
+        //Imprimir los tipos de datos en la consola
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+
+    }
+}
